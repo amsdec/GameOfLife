44fb0c2aea357f0b7cc0d2320eeebb1d2d39b88a
Alberto Montellano <alberto.montellano@tralix.com>
2014-10-24 21:17:30 -0500

    Green- kill live cell without neighbors

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -4,6 +4,13 @@
 public class GameOfLife {
 
     public String[][] nextGeneration(String[][] grid) {
+        for (int row = 0; row < grid.length; row++){
+            for(int column = 0; column < grid.length; column++) {
+                if(grid[row][column] != null) {
+                    grid[row][column] = null;
+                }
+            }
+        }
         return grid;
     }
 }
