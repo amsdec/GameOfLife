c4011e01bb5d9f52bb56aebcc570ac68e97fa864
Alberto Montellano <alberto.montellano@tralix.com>
2014-10-24 21:42:20 -0500

    Red- added a test to verify that next generation of one live cell with two neighbors must live

--- test/GameOfLifeTest.java
+++ test/GameOfLifeTest.java
@@ -25,4 +25,28 @@ public class GameOfLifeTest {
             }
         }
     }
+
+    @Test
+    public void nextGenerationOfOneLiveCellWithTwoNeighborsMustLive() {
+        String[][] originalGrid = new String[10][10];
+        originalGrid[0][0] = "X";
+        originalGrid[0][1] = "X";
+        originalGrid[1][1] = "X";
+        String[][] nextGeneration = game.nextGeneration(originalGrid);
+        Assert.assertArrayEquals(originalGrid, nextGeneration);
+        for (int row = 0; row < nextGeneration.length; row++){
+            for(int column = 0; column < nextGeneration.length; column++) {
+                if(row == 0 && column == 0){
+                    Assert.assertEquals("X",nextGeneration[row][column]);
+                }
+                if(row == 0 && column == 1){
+                    Assert.assertEquals("X",nextGeneration[row][column]);
+                }
+                if(row == 1 && column == 1){
+                    Assert.assertEquals("X",nextGeneration[row][column]);
+                }
+                Assert.assertNull(nextGeneration[row][column]);
+            }
+        }
+    }
 }
