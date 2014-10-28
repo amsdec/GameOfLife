8f30f437b69818018a671c30abf63808743a1f4b
Alberto Montellano <alberto.montellano@tralix.com>
2014-10-27 19:31:08 -0600

    Green- Bug fix- the problem was that i was returning always the same grid but modified, now to solve de bug i have created a new grid that represents the new generation

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -4,20 +4,23 @@
 public class GameOfLife {
 
     public CellsGrid nextGeneration(CellsGrid grid) {
+        CellsGrid newGeneration = new CellsGrid(grid.getRowCount(), grid.getColumnCount());
         for (int row = 0; row < grid.getRowCount(); row++) {
             for (int column = 0; column < grid.getColumnCount(); column++) {
                 if (grid.isCellInPositionLive(row, column)) {
                     if (cellShouldDie(grid, row, column)) {
-                        grid.killCellInPosition(row, column);
+                        newGeneration.killCellInPosition(row, column);
+                    } else {
+                        newGeneration.giveLiveToCellInPosition(row, column);
                     }
                 } else {
                     if (cellShouldBecomeLive(grid, row, column)) {
-                        grid.giveLiveToCellInPosition(row, column);
+                        newGeneration.giveLiveToCellInPosition(row, column);
                     }
                 }
             }
         }
-        return grid;
+        return newGeneration;
     }
 
     private boolean cellShouldBecomeLive(CellsGrid grid, int row, int column) {
